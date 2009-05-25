2bfab652c03ba94d5c39aca90597457e19f4a582
gyorgy.orban <gyorgy.orban@ba7b542e-3366-11de-ba44-d58fe8a9ce33>
2009-05-25 19:34:45 +0000

    MessageHandler example

---
+++ trunk/addressbook-handler-example/src/main/java/com/googlecode/cxf/protobuf/example/addressbook/Client.java
@@ -0,0 +1,64 @@
+package com.googlecode.cxf.protobuf.example.addressbook;
+
+import org.apache.cxf.endpoint.EndpointException;
+
+import com.example.tutorial.AddressBookProtos.AddressBook;
+import com.example.tutorial.AddressBookProtos.AddressBookService;
+import com.example.tutorial.AddressBookProtos.AddressBookServiceMessage;
+import com.example.tutorial.AddressBookProtos.AddressBookSize;
+import com.example.tutorial.AddressBookProtos.NamePattern;
+import com.example.tutorial.AddressBookProtos.Person;
+import com.google.protobuf.RpcCallback;
+import com.googlecode.cxf.protobuf.client.SimpleRpcChannel;
+import com.googlecode.cxf.protobuf.client.SimpleRpcController;
+
+/**
+ * Simple client for the addressbook service.
+ * 
+ * @author Gyorgy Orban
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 * @throws EndpointException
+	 */
+	public static void main(String[] args) throws EndpointException {
+		System.out.println("Create a person:\n");
+
+		Person.Builder person = Person.newBuilder();
+
+		person.setId(1);
+		person.setName("Alice");
+		Person alice = person.build();
+
+		System.out.println(alice);
+
+		AddressBookService service = AddressBookService
+				.newStub(new SimpleRpcChannel(
+						"http://localhost:8888/AddressBookService",
+						AddressBookServiceMessage.class));
+
+
+		SimpleRpcController controller = new SimpleRpcController();
+
+		System.out.println("Adding " + alice.getName() + " to the addressbook.");
+		service.addPerson(controller, alice, new RpcCallback<AddressBookSize>() {
+			public void run(AddressBookSize size) {
+				System.out.println("\nThere are " + size.getSize()
+						+ " person(s) in the address book now.");
+			}
+		});
+
+		controller.reset();
+
+		System.out.println("\nSearching for people with 'A' in their name.");
+		service.listPeople(controller, NamePattern.newBuilder().setPattern("A")
+				.build(), new RpcCallback<AddressBook>() {
+			public void run(AddressBook response) {
+				System.out.println("\nList of people found: \n" + response);
+			}
+		});
+	}
+
+}
