74cca0ac3c92402862ff47be1033d30e5cd1c6b5
gyorgy.orban <gyorgy.orban@ba7b542e-3366-11de-ba44-d58fe8a9ce33>
2009-06-03 18:29:33 +0000

    cleaned up imports

--- trunk/addressbook-handler-example/src/main/java/com/google/code/cxf/protobuf/example/addressbook/AddressBookMessageHandler.java
+++ trunk/addressbook-handler-example/src/main/java/com/google/code/cxf/protobuf/example/addressbook/AddressBookMessageHandler.java
@@ -1,7 +1,5 @@
 package com.google.code.cxf.protobuf.example.addressbook;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
