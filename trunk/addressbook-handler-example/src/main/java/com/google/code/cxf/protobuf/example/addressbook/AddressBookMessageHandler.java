bb34cf142e364ad75c0016ba5badd6dcc0919251
gyorgy.orban <gyorgy.orban@ba7b542e-3366-11de-ba44-d58fe8a9ce33>
2009-06-03 18:27:18 +0000

    improved message handler

--- trunk/addressbook-handler-example/src/main/java/com/google/code/cxf/protobuf/example/addressbook/AddressBookMessageHandler.java
+++ trunk/addressbook-handler-example/src/main/java/com/google/code/cxf/protobuf/example/addressbook/AddressBookMessageHandler.java
@@ -2,6 +2,8 @@ package com.google.code.cxf.protobuf.example.addressbook;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.example.tutorial.AddressBookProtos.AddressBook;
 import com.example.tutorial.AddressBookProtos.AddressBookServiceMessage;
@@ -12,7 +14,7 @@ import com.google.code.cxf.protobuf.ProtobufMessageHandler;
 import com.google.protobuf.Message;
 
 public class AddressBookMessageHandler implements ProtobufMessageHandler {
-	List<Person> records = new ArrayList<Person>();
+	Map<Integer, Person> records = new ConcurrentHashMap<Integer, Person>();
 
 	/**
 	 * @see com.googlecode.cxf.protobuf.ProtobufMessageHandler#handleMessage(com.google.protobuf.Message)
@@ -25,7 +27,7 @@ public class AddressBookMessageHandler implements ProtobufMessageHandler {
 			AddressBook.Builder addressbook = AddressBook.newBuilder();
 			NamePattern request = searchServiceMessage.getListPeople();
 
-			for (Person person : records) {
+			for (Person person : records.values()) {
 				if (person.getName().indexOf(request.getPattern()) >= 0) {
 					addressbook.addPerson(person);
 				}
@@ -34,8 +36,13 @@ public class AddressBookMessageHandler implements ProtobufMessageHandler {
 			return addressbook.build();
 		} else if (searchServiceMessage.hasField(AddressBookServiceMessage
 				.getDescriptor().findFieldByName("addPerson"))) {
+
 			Person request = searchServiceMessage.getAddPerson();
-			records.add(request);
+			if (records.containsKey(request.getId())) {
+				System.out.println("Warning: will replace existing person: "
+						+ records.get(request.getId()));
+			}
+			records.put(request.getId(), request);
 			return AddressBookSize.newBuilder().setSize(records.size()).build();
 		} else {
 			throw new RuntimeException("Payload not found in message "
